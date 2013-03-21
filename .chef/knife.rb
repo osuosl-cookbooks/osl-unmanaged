e6fe7f3f4feb3e344b5decaf19d941abf5837fe0
Rudy Grigar <rudy@osuosl.org>
2013-03-21 21:12:50 +0000

    OSL knife config and default aliases

---
+++ .chef/knife.rb
@@ -0,0 +1,24 @@
+base_path   = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+user_email  = `git config --get user.email`
+home_dir    = ENV['HOME'] || ENV['HOMEDRIVE']
+org         = ENV['chef_org'] || 'my_org'
+
+chef_server_url          'https://osl-chef.osuosl.test'
+log_level                :info
+log_location             STDOUT
+
+# USERNAME is UPPERCASE in Windows, but lowercase in the Chef server,
+# so `downcase` it.
+node_name                ( ENV['USER'] || ENV['USERNAME'] ).downcase
+client_key               "#{home_dir}/.chef/#{node_name}.pem"
+validation_client_name   'chef-validator'
+validation_key           "#{home_dir}/.chef/#{validation_client_name}.pem"
+cache_type               'BasicFile'
+cache_options( :path => "#{home_dir}/.chef/checksums" )
+
+cookbook_path            ["#{base_path}/cookbooks"]
+cookbook_copyright       "OSU Open Source Lab"
+cookbook_license         "none"
+cookbook_email           "#{user_email}"
+role_path            	 ["#{base_path}/roles"]
+data_bag_path            ["#{base_path}/data_bags"]
---
+++ cookbooks/aliases/metadata.rb
@@ -0,0 +1,8 @@
+name             'aliases'
+maintainer       'OSU Open Source Lab'
+maintainer_email ''
+license          'All rights reserved'
+description      'Installs/Configures aliases'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'magic_shell', '~> 0.2.0'
