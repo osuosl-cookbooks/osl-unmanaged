fcf6fde77fdcb5528b738df1df5ca9eeaac127dc
Rudy Grigar <rudy@grigar.net>
2013-04-03 21:37:21 -0700

    add root user config

--- cookbooks/base/attributes/default.rb
+++ cookbooks/base/attributes/default.rb
@@ -1,4 +1,4 @@
 case node['platform']
 when "redhat","centos","scientific","amazon"
-  default['base']['packages'] = %w[ bwm-ng dmidecode htop iotop pwgen screen strace time ]
+  default['base']['packages'] = %w[ bwm-ng dmidecode htop iotop pwgen screen strace time ruby-shadow ]
 end
