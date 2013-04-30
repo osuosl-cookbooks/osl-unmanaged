dc85920707b841f0e811f38ab18d996fffc61fee
Rudy Grigar <rudy@grigar.net>
2013-04-29 19:19:55 -0700

    restart sshd when updating this template

--- osl-cookbooks/ssh-ldap/recipes/default.rb
+++ osl-cookbooks/ssh-ldap/recipes/default.rb
@@ -9,13 +9,15 @@
 include_recipe "openssh"
 
 # Manage ssh ldap configuration
+package "openssh-ldap" do
+  action :install
+end
+
 template "/etc/ssh/ldap.conf" do
   source "ldap.conf.erb"
   mode 0644
   owner "root"
   group "root"
+  notifies :restart, "service[ssh]"
 end
 
-package "openssh-ldap" do
-  action :install
-end
