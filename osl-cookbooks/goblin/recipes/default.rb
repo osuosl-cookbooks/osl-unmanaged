629abd045758f07bce0715f9b987231bf9e549e9
Rudy Grigar <rudy@grigar.net>
2013-05-30 15:05:52 -0700

    clean up the include_recipes for goblin

--- osl-cookbooks/goblin/recipes/default.rb
+++ osl-cookbooks/goblin/recipes/default.rb
@@ -7,7 +7,34 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "goblin::django"
+# Django goblin application is installed by default on all nodes.
+application "goblin" do
+  path "/var/www/goblin"
+  owner "root"
+  group "www-data"
+  repository "https://github.com/osuosl/Goblin.git"
+  revision "master"
+  migrate true
+  packages ["libpq-dev", "git-core", "libsasl2-dev", "libldap2-dev",
+    "python2.6-dev", "libapache2-mod-auth-cas"]
+
+  django do
+    requirements "requirements/requirements.txt"
+    settings_template "settings.py.erb"
+    debug true
+    database do
+      database "goblin"
+      engine "sqlite3"
+    end
+    database_master_role "goblin_backend"
+  end
+end
+
+git "/var/www/goblin/shared/env/lib/python2.6/site-packages/googleimap" do
+  repository "git://github.com/osuosl/google-imap.git"
+  reference "master"
+  action :sync
+end
 
 %w{moreutils libsasl2-dev libldap2-dev python2.6-dev ldap-utils libnet-oauth-perl libmail-imapclient-perl}.each do |pkg|
   package pkg
--- osl-cookbooks/goblin/recipes/django.rb
+++ osl-cookbooks/goblin/recipes/django.rb
@@ -7,38 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "goblin"
-
-application "goblin" do
-  path "/var/www/goblin"
-  owner "root"
-  group "www-data"
-  repository "https://github.com/osuosl/Goblin.git"
-  revision "master"
-  migrate true
-  packages ["libpq-dev", "git-core", "libsasl2-dev", "libldap2-dev",
-    "python2.6-dev", "libapache2-mod-auth-cas"]
-
-  django do
-    requirements "requirements/requirements.txt"
-    settings_template "settings.py.erb"
-    debug true
-    database do
-      database "goblin"
-      engine "sqlite3"
-    end
-    database_master_role "goblin_backend"
-  end
-end
-
-git "/var/www/goblin/shared/env/lib/python2.6/site-packages/googleimap" do
-  repository "git://github.com/osuosl/google-imap.git"
-  reference "master"
-  action :sync
-end
+include_recipe "goblin::default"
 
 web_app "goblin" do
   server_name node['hostname']
   template "apache.erb"
 end
-
