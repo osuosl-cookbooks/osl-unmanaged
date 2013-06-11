41d061e006f7cb0a01c7824b7a1ab8a5477c0133
Justin Dugger <jldugger@osuosl.org>
2013-06-11 14:35:51 -0700

    map databag to variables

--- osl-cookbooks/goblin/attributes/django.rb
+++ osl-cookbooks/goblin/attributes/django.rb
@@ -10,3 +10,8 @@ default['goblin']['django']['adminmail'] = "root@osuosl.org"
 default['goblin']['django']['password'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["ldap"]["login"]
 default['goblin']['ldap']['password'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["ldap"]["password"]
 default['goblin']['ldap']['username'] = "uid=onid_googlesync,ou=specials,o=orst.edu"
+default['goblin']['google']['secret'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["google"]["login"]
+default['goblin']['google']['login'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["google"]["secret"]
+default['goblin']['google']['key'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["google"]["key"]
+default['goblin']['rabbitmq'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["rabbitmq"]
+
