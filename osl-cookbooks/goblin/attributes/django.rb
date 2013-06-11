d4436a7a22efd4dc85ae1d0c2dff0fb63a897a24
Justin Dugger <jldugger@osuosl.org>
2013-06-11 14:47:48 -0700

    missed a few template variables

--- osl-cookbooks/goblin/attributes/django.rb
+++ osl-cookbooks/goblin/attributes/django.rb
@@ -14,4 +14,5 @@ default['goblin']['google']['secret'] = Chef::EncryptedDataBagItem.load("goblin"
 default['goblin']['google']['login'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["google"]["secret"]
 default['goblin']['google']['key'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["google"]["key"]
 default['goblin']['rabbitmq'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["rabbitmq"]
-
+default['goblin']['cyrus']['username'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["cyrus"]["login"]
+default['goblin']['cyrus']['password'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["cyrus"]["password"]
