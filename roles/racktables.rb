8ab667ed573a7469fcd32e85e797da6acff5ba5b
Trevor Bramwell <bramwelt@osuosl.org>
2013-06-14 11:32:56 -0700

    Role: Racktables - Added apache2 recipes
    Added recipes:
      * apache2
      * apache2 mod_ssl
      * apache2 mod_php5

--- roles/racktables.rb
+++ roles/racktables.rb
@@ -3,5 +3,8 @@ description "ermahgerd racktables"
 run_list(
   "role[base_managed]",
   "recipe[firewall::http]",
+  "recipe[apache2]",
+  "recipe[apache2::mod_ssl]",
+  "recipe[apache2::mod_php5]",
   "recipe[racktables]"
 )
