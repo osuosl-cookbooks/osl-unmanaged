4e1b886276fcaf6bda54d20626fd3d9be124cc70
Justin Dugger <jldugger@osuosl.org>
2013-06-26 17:52:42 -0700

    support overriding connection strings

--- osl-cookbooks/goblin/attributes/django.rb
+++ osl-cookbooks/goblin/attributes/django.rb
@@ -8,12 +8,23 @@
 default['goblin']['django']['adminname'] = "OSL Admin"
 default['goblin']['django']['adminmail'] = "root@osuosl.org"
 default['goblin']['django']['password'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["ldap"]["login"]
+
 default['goblin']['ldap']['password'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["ldap"]["password"]
 default['goblin']['ldap']['username'] = "uid=onid_googlesync,ou=specials,o=orst.edu"
+
 default['goblin']['google']['secret'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["google"]["secret"]
 default['goblin']['google']['login'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["google"]["login"]
 default['goblin']['google']['key'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["google"]["key"]
 default['goblin']['google']['password'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["google"]["password"]
+default['goblin']['google']['domain'] = "gtest.onid.oregonstate.edu"
+
+default['goblin']['rabbitmq']['host'] = "test-migbe.onid.oregonstate.edu"
 default['goblin']['rabbitmq'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["rabbitmq"]
+
 default['goblin']['cyrus']['username'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["cyrus"]["login"]
 default['goblin']['cyrus']['password'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["cyrus"]["password"]
+default['goblin']['cyrus']['host'] = "test-cyrus-fe.onid.oregonstate.edu"
+
+default['goblin']['memcache']['host'] = "test-migfe.onid.oregonstate.edu:11211"
+
+
