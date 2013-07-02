d3b0fe14ae0d893feacbdc1a51eb3a885bf4e9e0
Alan Sherman <alan_s@osuosl.org>
2013-07-01 20:31:39 -0700

    changed version of pg

--- roles/postgres.rb
+++ roles/postgres.rb
@@ -2,10 +2,19 @@ name "postgres"
 description "postgresql server role"
 run_list(
     "role[base_managed]",
-    "recipe[postgresql]",
+    "recipe[postgresql::server]",
     "recipe[firewall::postgres]"
 )
 default_attributes(
 )
 override_attributes(
+  "postgresql" => {
+    "version" => "9.2",
+    "config" => {
+      "listen_addresses" => "*",
+      "max_connections" => "600",
+      "shared_buffers" => "1024MB",
+      "log_filename" => "postgresql-%Y-%m-%d.log"
+    }
+  }
 )
