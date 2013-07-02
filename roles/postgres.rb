8ef0ad96a30bec123e329b722f58620328b6c852
Rudy Grigar <rudy@grigar.net>
2013-07-01 20:38:18 -0700

    update postgres settings

--- roles/postgres.rb
+++ roles/postgres.rb
@@ -10,6 +10,18 @@ default_attributes(
 override_attributes(
   "postgresql" => {
     "version" => "9.2",
+    "enable_pgdg_yum" => true,
+    "dir" => "/var/lib/pgsql/9.2/data",
+    "client" => {
+      "packages" => ["postgresql92", "postgresql92-devel"]
+    },
+    "server" => {
+      "packages" => ["postgresql92-server"],
+      "service_name" => "postgresql-9.2"
+    },
+    "contrib" => {
+      "packages" => ["postgresql92-contrib"]
+    },
     "config" => {
       "listen_addresses" => "*",
       "max_connections" => "600",
