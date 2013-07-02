9577fbc5be9d18f120ce9c628754f73245a12dee
Alan Sherman <alan_s@osuosl.org>
2013-07-01 18:48:36 -0700

    fixed postgres role syntax

--- roles/postgres.rb
+++ roles/postgres.rb
@@ -1,17 +1,11 @@
-{
-  "override_attributes": {
-  },
-  "json_class": "Chef::Role",
-  "description": "postgres server role",
-  "name": "postgres",
-  "chef_type": "role",
-  "default_attributes": {
-  },
-  "run_list": [
+name "postgres"
+description "postgresql server role"
+run_list(
     "role[base_managed]",
     "recipe[postgresql]",
     "recipe[firewall::postgres]"
-  ],
-  "env_run_lists": {
-  }
-}
+)
+default_attributes(
+)
+override_attributes(
+)
