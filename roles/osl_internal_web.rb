95ce69af01e9de498af1175c1a2f069e637a5a5f
Rudy Grigar <rudy@grigar.net>
2013-07-03 09:54:18 -0700

    add internal web role

---
+++ roles/osl_internal_web.rb
@@ -0,0 +1,21 @@
+name "osl_internal_web"
+description "ermahgerd internal web services"
+run_list(
+  "role[base_managed]",
+  "recipe[firewall::http]",
+  "recipe[apache2]",
+  "recipe[apache2::mod_ssl]",
+  "recipe[apache2::mod_php5]",
+  "recipe[racktables]",
+  "recipe[java::openjdk]",
+  "recipe[jenkins]",
+  "recipe[jenkins::proxy_apache2]"
+)
+override_attributes(
+  "jenkins" => {
+    "http_proxy" => {
+      "variant" => "apache2",
+      "host_name" => "jenkins.osuosl.org"
+    }
+  }
+)
