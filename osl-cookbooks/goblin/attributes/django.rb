3161ecffb206d6d9ef46cfc798f54d546b5f3199
Justin Dugger <jldugger@osuosl.org>
2013-07-03 10:09:33 -0700

    simplify databag configuration, in prep for a prod/pprd split

--- osl-cookbooks/goblin/attributes/django.rb
+++ osl-cookbooks/goblin/attributes/django.rb
@@ -5,24 +5,27 @@
 # Copyright 2013, OSU Open Source Lab
 #
 #
+
+databag = Chef::EncryptedDataBagItem.load("goblin","credentials")
+
 default['goblin']['django']['adminname'] = "OSL Admin"
 default['goblin']['django']['adminmail'] = "root@osuosl.org"
-default['goblin']['django']['password'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["ldap"]["login"]
+default['goblin']['django']['password'] = databag["ldap"]["login"]
 
-default['goblin']['ldap']['password'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["ldap"]["password"]
+default['goblin']['ldap']['password'] = databag["ldap"]["password"]
 default['goblin']['ldap']['username'] = "uid=onid_googlesync,ou=specials,o=orst.edu"
 
-default['goblin']['google']['secret'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["google"]["secret"]
-default['goblin']['google']['login'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["google"]["login"]
-default['goblin']['google']['key'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["google"]["key"]
-default['goblin']['google']['password'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["google"]["password"]
+default['goblin']['google']['secret'] = databag["google"]["secret"]
+default['goblin']['google']['login'] = databag["google"]["login"]
+default['goblin']['google']['key'] = databag["google"]["key"]
+default['goblin']['google']['password'] = databag["google"]["password"]
 default['goblin']['google']['domain'] = "gtest.onid.oregonstate.edu"
 
 default['goblin']['rabbitmq']['host'] = "test-migbe.onid.oregonstate.edu"
-default['goblin']['rabbitmq']['password'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["rabbitmq"]
+default['goblin']['rabbitmq']['password'] = databag["rabbitmq"]
 
-default['goblin']['cyrus']['username'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["cyrus"]["login"]
-default['goblin']['cyrus']['password'] = Chef::EncryptedDataBagItem.load("goblin","credentials")["cyrus"]["password"]
+default['goblin']['cyrus']['username'] = databag["cyrus"]["login"]
+default['goblin']['cyrus']['password'] = databag["cyrus"]["password"]
 default['goblin']['cyrus']['host'] = "test-cyrus-fe.onid.oregonstate.edu"
 
 default['goblin']['memcache']['host'] = "test-migfe.onid.oregonstate.edu:11211"
