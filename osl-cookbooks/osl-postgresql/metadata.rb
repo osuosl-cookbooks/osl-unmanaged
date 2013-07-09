d661bf50534b4f584dbbae3b0ec7d8913b508092
Geoffrey Corey <coreyg@osuosl.org>
2013-07-09 16:51:07 -0700

    Adding a recipe that manages the hba of postgres

---
+++ osl-cookbooks/osl-postgresql/metadata.rb
@@ -0,0 +1,8 @@
+name             'osl-postgresql'
+maintainer       'OSU Open Source Lab'
+maintainer_email 'coreyg@osuosl.org
+'
+license          'All rights reserved'
+description      'Installs/Configures osl-postgresql'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
