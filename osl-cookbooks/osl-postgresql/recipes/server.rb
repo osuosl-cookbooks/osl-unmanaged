96ffeff83f9bfb709e3e219aee32830f90a1f0cf
Rudy Grigar <rudy@grigar.net>
2013-07-10 13:30:52 -0700

    clean up osl-postgres...

--- osl-cookbooks/osl-postgresql/recipes/server.rb
+++ osl-cookbooks/osl-postgresql/recipes/server.rb
@@ -1,7 +1,7 @@
-template "#{node['postgresql']['dir']}/pg_hba.conf" do
-  source "pg_hba.conf.erb"
-  owner "postgres"
-  group "postgres"
-  mode 00600
-  notifies :reload, 'service[postgresql]', :immediately
-end
+#template "#{node['postgresql']['dir']}/pg_hba.conf" do
+#  source "pg_hba.conf.erb"
+#  owner "postgres"
+#  group "postgres"
+#  mode 00600
+#  notifies :reload, 'service[postgresql]', :immediately
+#end
