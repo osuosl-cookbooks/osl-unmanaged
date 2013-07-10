4a0d88a3b80d1434e6ad12c9eca23d989bafe32b
Geoffrey Corey <coreyg@osuosl.org>
2013-07-10 10:35:26 -0700

    Finished adding a a recipe to modify pg_hba.conf in the postgresql cookbook, and can be modified to have rules setup for different postgresql hosts

--- osl-cookbooks/osl-postgresql/attributes/default.rb
+++ osl-cookbooks/osl-postgresql/attributes/default.rb
@@ -1,6 +1,10 @@
+# get the postgresql attribute information
+include_attribute "postgresql"
+
 default['postgresql']['pg_hba'] = [
   {:type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'ident'},
   {:type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'ident'},
   {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5'},
-  {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'}
+  {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'},
+  {:type => 'host', :db => 'fsslgy_redmine', :user => 'fsslgy_redmine', :addr => '140.211.15.250/32', :method => 'md5'}
 ]
