9bd50cf4b0f1770ff2142157301ab8cfd093ee84
Geoffrey Corey <coreyg@osuosl.org>
2013-07-09 17:21:07 -0700

    Doing a redmine cookbook the proper way from the beginning

---
+++ osl-cookbooks/osl-redmine/metadata.rb
@@ -0,0 +1,9 @@
+name             'osl-redmine'
+maintainer       'OSU Open Source Lab'
+maintainer_email 'coreyg@osuosl.org
+'
+license          'All rights reserved'
+description      'Installs/Configures osl-redmine'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
