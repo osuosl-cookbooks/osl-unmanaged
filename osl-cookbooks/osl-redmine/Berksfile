adbad3e30405165f96a9bb982022beb83fe2a6ff
Geoffrey Corey <coreyg@osuosl.org>
2013-07-11 13:50:26 -0700

    Starting from a known working cookbook.

---
+++ osl-cookbooks/osl-redmine/Berksfile
@@ -0,0 +1,6 @@
+site :opscode
+
+metadata
+
+cookbook 'apt'
+cookbook 'yum', '= 2.2.0'
---
+++ osl-cookbooks/osl-redmine/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: redmine
+# Recipe:: default
+#
+# Copyright 2012, Juanje Ojeda <juanje.ojeda@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# set through recipes the base system
+case node['platform']
+when "redhat","centos","amazon","scientific","fedora","suse"
+  include_recipe "redmine::source"
+when "debian","ubuntu"
+  case node["redmine"]["install_method"]
+  when "source"
+    include_recipe "redmine::source"
+  when "package"
+    include_recipe "redmine::package"
+  end
+end
