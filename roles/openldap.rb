c9e5992b1c1c03fb76eee64396ce61d95d1fd92c
Rudy Grigar <rudy@grigar.net>
2013-07-13 16:44:35 -0700

    update the openldap role to configure the server

--- roles/openldap.rb
+++ roles/openldap.rb
@@ -1,10 +1,16 @@
 name "openldap"
 description "ldap server role"
 run_list(
-  "recipe[openldap::server]"
+  "recipe[osl-slapd]"
 )
 override_attributes(
-  "openldap" => {
-    "rootpw" => "{SSHA}aP1JdkMLJwIIFLZJCuP0A3HLAOnXa3kZ"
+  "osl-slapd" => {
+    "rootpw" => "{SSHA}45XuCvh2KTCJNJuDDx3o7QsSSGypQ4tm",
+    "suffix" => "dc=osuosl,dc=org",
+    "access" => [
+      'access to attrs=userPassword by group.exact="cn=Admin,dc=osuosl,dc=org" write by dn="uid=syncuser,dc=osuosl,dc=org" read by anonymous auth by self write by * none',
+      'access to attrs=mail,telephoneNumber,facsimileTelephoneNumber,mobile,sshPublicKey by group.exact="cn=Admin,dc=osuosl,dc=org" write by dn="uid=syncuser,dc=osuosl,dc=org" read by self write by * read',
+      'access to * by group.exact="cn=Admin,dc=osuosl,dc=org" write by dn="uid=syncuser,dc=osuosl,dc=org" read by * read'
+    ]
   }
 )
