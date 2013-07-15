562e72e029790d55396d9e8a4c659a49ca833031
Rudy Grigar <rudy@grigar.net>
2013-07-15 10:52:49 -0700

    add openldap firewall rule

--- osl-cookbooks/firewall/recipes/openldap.rb
+++ osl-cookbooks/firewall/recipes/openldap.rb
@@ -1,15 +1,15 @@
 #
 # Cookbook Name:: firewall
-# Recipe:: mysql
+# Recipe:: openldap
 #
 # Copyright 2013, OSU Open Source Lab
 #
 # All rights reserved - Do Not Redistribute
 #
 
-# Allow MySQL
-simple_iptables_rule "mysql" do
-  rule "--proto tcp --dport 3306"
+# Allow OpenLDAP
+simple_iptables_rule "openldap" do
+  rule "--proto tcp --dport 636"
   jump "ACCEPT"
 end
 
