cc17f45aecc5fabf515ebe531743f01b62e922ec
Geoffrey Corey <coreyg@osuosl.org>
2013-07-15 10:24:00 -0700

    Adding vanilla redmine cookbook, need to modify it for mysql1-vip and mysql-percona

---
+++ osl-cookbooks/osl-redmine/metadata.rb
@@ -0,0 +1,18 @@
+name             "redmine"
+maintainer       "Juanje Ojeda"
+maintainer_email "juanje.ojeda@gmail.com"
+license          "Apache 2.0"
+description      "Install Redmine from Github"
+version          "0.0.1"
+
+recipe "redmine", "Install the Redmine application from the source"
+recipe "redmine::source", "Install the Redmine application from the source"
+recipe "redmine::package", "Install the Redmine application from packages"
+
+%w{ git apache2 passenger_apache2 mysql postgresql apt yum database}.each do |dep|
+  depends dep
+end
+
+%w{ debian ubuntu centos redhat amazon scientific fedora suse }.each do |os|
+    supports os
+end
