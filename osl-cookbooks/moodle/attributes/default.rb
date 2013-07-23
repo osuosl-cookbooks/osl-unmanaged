b4be23fd2d31dd9c8eae805e4cd7afaeea07d466
armiller <armiller@osuosl.org>
2013-07-22 17:13:09 -0700

    For now disable any calls to mysql ruby

--- osl-cookbooks/moodle/attributes/default.rb
+++ osl-cookbooks/moodle/attributes/default.rb
@@ -1,3 +1,4 @@
 default.moodle[:database] = 'moodle'
 default.moodle[:data_dir] = '/var/lib/moodle'
 default.moodle[:url]      = 'http://localhost'
+default.moodle[:gitrepo]  = 'git://git.moodle.org/moodle.git'
---
+++ osl-cookbooks/moodle/recipes/moodle19.rb
@@ -0,0 +1,104 @@
+include_recipe "mysql::server" 
+#include_recipe "mysql::ruby"
+include_recipe "apache2::mod_php5" 
+include_recipe "php::module_mysql"
+
+%w[
+  aspell
+  git
+  graphviz
+].each{|p| package(p) }
+
+### Local vars ###
+moodle_version = '1.9.6'
+moodle_dir = "moodle-#{moodle_version}"
+
+
+## Prepare
+mysql_database node.moodle['database'] do
+  connection :host => "localhost", :username => 'root', :password => node['mysql']['server_root_password']
+  encoding 'UTF8'
+  action :create
+end
+
+directory node.moodle.data_dir do
+  owner 'apache'
+  group 'apache'
+  mode 0777
+end
+
+directory "/var/www/moodle" do
+  owner 'apache'
+  owner 'apache'
+  mode 0755
+  action :create
+end
+
+###
+###  WARNING: vboxfs is really clunky and fails in many ways. There is a reason
+###           why I'm not working directly in the /vagrant folder.
+###
+
+git "/var/www/moodle" do
+    repository 'git://git.moodle.org/moodle.git'
+    revision 'MOODLE_19_STABLE' 
+    action :checkout
+end
+
+
+# install.php doesn't create the config.php file when run in /vagrant
+bash "configure-moodle" do
+  cwd "/usr/local/moodle"
+  code <<-CODE
+    set -e
+    chmod a+w .
+    sudo -u www-data /usr/bin/php admin/cli/install.php \
+      --non-interactive \
+      --lang=en \
+      --wwwroot=#{node.moodle['url']} \
+      --dataroot=#{node.moodle['data_dir']} \
+      --dbuser=root \
+      --dbpass="#{node['mysql']['server_root_password']}" \
+      --adminuser=admin \
+      --adminpass=adminpass \
+      --fullname="TestSite" \
+      --shortname="test" \
+      --agree-license > #{node.moodle['data_dir']}/php-install.log 2>&1
+    chmod 644 config.php
+    chmod 755 .
+  CODE
+  creates '/usr/local/moodle/moodle/config.php'
+end
+
+# tar claims /vagrant is read-only when unpacking
+# so we move all this afterwards
+# bash "move-to-vagrant" do
+#   source = "/usr/local/#{moodle_dir}"
+#   target = "/vagrant/#{moodle_dir}"
+#   code <<-CODE
+#     set -e
+#     if [ -d #{target} ]; then
+#       # Already exists. Instead we're going to re-provision the DB
+#       sudo -u www-data /usr/bin/php #{target}/moodle/admin/cli/install_database.php
+#     else
+#       cp -r #{source} /vagrant
+#     fi
+#     rm -rf #{source}
+#     ln -s #{target} #{source}
+#   CODE
+#   only_if { File.directory?('/vagrant') && File.directory?("/usr/local/#{moodle_dir}") }
+# end
+
+cron "moodle-maintenance" do
+  minute '*/15'
+  user 'www-data'
+  command "/usr/bin/php /usr/local/moodle/moodle/admin/cli/cron.php"
+end
+
+apache_site 'default' do
+  enable false
+end
+
+web_app 'moodle' do
+  template 'moodle.conf.erb'
+end
