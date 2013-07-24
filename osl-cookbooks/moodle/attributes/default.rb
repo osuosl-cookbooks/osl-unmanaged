cf06e23eed4c57aafe12708bada7082d560c99e7
armiller <armiller@osuosl.org>
2013-07-24 14:06:47 -0700

    Moodle19 does not have acces to cli

--- osl-cookbooks/moodle/attributes/default.rb
+++ osl-cookbooks/moodle/attributes/default.rb
@@ -1,4 +1,4 @@
 default.moodle[:database] = 'moodle'
-default.moodle[:data_dir] = '/var/lib/moodle'
+default.moodle[:data_dir] = '/var/moodledata'
 default.moodle[:url]      = 'http://localhost'
 default.moodle[:gitrepo]  = 'git://git.moodle.org/moodle.git'
