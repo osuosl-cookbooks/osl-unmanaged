32837cbef32d95df6ed54d60329ecaf7d9a6c0b4
Alan Sherman <alan_s@osuosl.org>
2013-08-02 14:53:45 -0700

    firewall rules for teeworlds

---
+++ osl-cookbooks/lanparty/recipes/teeworlds.rb
@@ -0,0 +1,6 @@
+# Allow incoming teeworlds traffic
+simple_iptables_rule "teeworlds" do
+    rule ["--proto udp --dport #{node['lanparty']['teewrolds']['net_port']}",
+          "--proto tcp --dport #{node['lanparty']['teeworlds']['net_port']}"]
+    jump "ACCEPT"
+end
