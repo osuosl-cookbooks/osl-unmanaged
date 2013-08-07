abc00fe7e5afe014338136cee8ed76070c7dbc6b
Rudy Grigar <rudy@grigar.net>
2013-08-07 10:39:58 -0700

    more updates to osl-postgresql

--- osl-cookbooks/osl-postgresql/metadata.rb
+++ osl-cookbooks/osl-postgresql/metadata.rb
@@ -1,8 +1,7 @@
 name             'osl-postgresql'
 maintainer       'OSU Open Source Lab'
-maintainer_email 'coreyg@osuosl.org
-'
+maintainer_email 'coreyg@osuosl.org'
 license          'All rights reserved'
 description      'Installs/Configures osl-postgresql'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.2'
