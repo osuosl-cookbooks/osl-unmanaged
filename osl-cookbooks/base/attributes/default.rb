ed2ac78769b9e0d1d0337d20baa434cc91620480
Rudy Grigar <rudy@grigar.net>
2013-08-07 13:40:23 -0700

    add vim and bash-completion to base packages

--- osl-cookbooks/base/attributes/default.rb
+++ osl-cookbooks/base/attributes/default.rb
@@ -1,6 +1,6 @@
 case node['platform_family']
 when "rhel"
-  default['base']['packages'] = %w[ bwm-ng cronie cronie-anacron crontabs dmidecode htop iotop pv pwgen screen strace time ]
+  default['base']['packages'] = %w[ vim bash-completion bwm-ng cronie cronie-anacron crontabs dmidecode htop iotop pv pwgen screen strace time ]
 when "debian"
   default['base']['packages'] = %w[ bwm-ng cron dmidecode htop iotop pv pwgen screen strace time ]
 end
--- osl-cookbooks/base/metadata.rb
+++ osl-cookbooks/base/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rudy@osuosl.org'
 license          'All rights reserved'
 description      'Installs/Configures base packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 depends          'chef_handler'
 depends          'user'
 depends          'openssh'
