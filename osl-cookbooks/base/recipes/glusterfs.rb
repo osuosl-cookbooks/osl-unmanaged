a47475e32224be8a8b52c61022d09bd208d311d7
Rudy Grigar <rudy@grigar.net>
2013-08-21 08:31:48 -0700

    gluster: use the epel community repositories for gluster

--- osl-cookbooks/base/recipes/glusterfs.rb
+++ osl-cookbooks/base/recipes/glusterfs.rb
@@ -7,11 +7,22 @@
 # All rights reserved - Do Not Redistribute
 
 
-# Configure the glusterfs yum repository
+# Configure the glusterfs yum repositories
 yum_repository "glusterfs" do
-  repo_name "glusterfs"
-  description "GlusterFS repo $releasever - $basearch"
-  url "http://packages.osuosl.org/repositories/centos-$releasever/glusterfs/$basearch"
+  action :remove
+end
+
+yum_repository "glusterfs-epel" do
+  repo_name "glusterfs-epel"
+  description "GlusterFS is a clustered file-system capable of scaling to several petabytes."
+  url "http://download.gluster.org/pub/gluster/glusterfs/3.4/3.4.0/EPEL.repo/epel-$releasever/$basearch/"
+  action :add
+end
+
+yum_repository "glusterfs-noarch-epel" do
+  repo_name "glusterfs-noarch-epel"
+  description "GlusterFS is a clustered file-system capable of scaling to several petabytes."
+  url "http://download.gluster.org/pub/gluster/glusterfs/3.4/3.4.0/EPEL.repo/epel-$releasever/noarch"
   action :add
 end
 
