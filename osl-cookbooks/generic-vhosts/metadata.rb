fd77ede671192e91a126ea26ef0202d3bf1d0749
armiller <armiller@osuosl.org>
2013-08-26 14:18:47 -0700

    Adding generic-vhosts cookbook

---
+++ osl-cookbooks/generic-vhosts/metadata.rb
@@ -0,0 +1,8 @@
+name             'generic-vhosts'
+maintainer       'OSU Open Source Lab'
+maintainer_email 'systems@osuosl.org'
+license          'Apache 2.0'
+description      'Installs/Configures generic-vhosts'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'apache2'
