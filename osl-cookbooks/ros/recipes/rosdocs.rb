b3817efe77c381dc3982dc00b47c9a50af8160ba
Rudy Grigar <rudy@grigar.net>
2013-08-27 13:31:32 -0700

    add more aliases just in case

--- osl-cookbooks/ros/recipes/rosdocs.rb
+++ osl-cookbooks/ros/recipes/rosdocs.rb
@@ -28,7 +28,9 @@ node.set['ros-docs']['glustervol'] = 'fs3.osuosl.bak:/ros-docs'
 # Apache configuration
 node.set['ros-docs']['server_name'] = 'docs.ros.org'
 node.set['ros-docs']['server_aliases'] = [
-  'docs.ros.osuosl.org'
+  'doc.ros.org',
+  'docs.ros.osuosl.org',
+  'doc.ros.osuosl.org'
 ]
 
 # Include the relevant recipes
