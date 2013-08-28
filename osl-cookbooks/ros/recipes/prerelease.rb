ba81442b7da5d997c8b5aacd6e61f3ad120a2170
Daniel Takamori <pono@osuosl.org>
2013-08-27 17:34:02 -0700

    ros prerelease things

--- osl-cookbooks/ros/recipes/prerelease.rb
+++ osl-cookbooks/ros/recipes/prerelease.rb
@@ -1,7 +1,7 @@
-include_recipe git
-include_recipe apache2
-include_recipe apache2::mod_wsgi
-include_recipe python
+include_recipe 'git'
+include_recipe 'apache2'
+include_recipe 'apache2::mod_wsgi'
+include_recipe 'python'
 
 file '/var/www/prerelease_website/prerelease_website/logfile' do
     action :create
