c8971b5fe4e83616649af9a84b9dd771272a455f
armiller <armiller@osuosl.org>
2013-08-30 15:00:02 -0700

    Adding dynamic max clients for apache

--- osl-cookbooks/generic-vhosts/attributes/default.rb
+++ osl-cookbooks/generic-vhosts/attributes/default.rb
@@ -1,1 +1,11 @@
 default['apache']['default_site_enabled'] = false
+
+total_memory = node['memory']['total']
+
+mem = (total_memory.split("kB")[0].to_i / 1024) # in MB
+
+max_clients = ((mem - 512) / 150).floor
+
+if max_clients < 5 do
+    max_clients = 5
+end
