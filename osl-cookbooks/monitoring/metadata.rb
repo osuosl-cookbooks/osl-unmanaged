3c22a16420554eed9a90aa997987f1222148018a
Rudy Grigar <rudy@grigar.net>
2013-09-03 16:35:49 -0700

    bump monitoring

--- osl-cookbooks/monitoring/metadata.rb
+++ osl-cookbooks/monitoring/metadata.rb
@@ -5,5 +5,6 @@ maintainer_email 'rudy@grigar.net
 license          'All rights reserved'
 description      'Installs/Configures monitoring'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 depends          'nagios'
+depends          'munin'
