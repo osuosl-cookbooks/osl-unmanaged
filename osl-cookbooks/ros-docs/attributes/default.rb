8e7ce5ef722ea75aeb9d19c7bcfaf966eaf54e86
Bryon <bryon@osuosl.org>
2013-09-10 10:59:51 -0700

    added localmount options to ros-docs

--- osl-cookbooks/ros-docs/attributes/default.rb
+++ osl-cookbooks/ros-docs/attributes/default.rb
@@ -3,12 +3,16 @@ default['ros-docs']['user'] = "rosbot"
 default['ros-docs']['ssh_keys'] = nil
 default['ros-docs']['linkpath'] = "/home/#{node['ros-docs']['user']}/docs"
 
+# Localmount config
+default['ros-docs']['localmount'] = nil
+default['ros-docs']['mountpath'] = "/data"
+
 # Gluster config
 default['ros-docs']['glustervol'] = nil
-default['ros-docs']['glusterpath'] = "/data"
+default['ros-docs']['glusterpath'] = node['ros-docs']['mountpath']
 
 # Apache2 config
 default['ros-docs']['server_name'] = node['fqdn']
 default['ros-docs']['server_aliases'] = nil
-default['ros-docs']['docroot'] = "#{node['ros-docs']['glusterpath']}/docs"
+default['ros-docs']['docroot'] = "#{node['ros-docs']['mountpath']}/docs"
 default['ros-docs']['logs_dir'] = "/var/log/httpd/#{node['ros-docs']['server_name']}"
