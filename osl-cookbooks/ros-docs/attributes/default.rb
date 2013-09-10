91758237feffac6a1a5bb5c7b1790150fafd8d0e
Rudy Grigar <rudy@grigar.net>
2013-09-10 15:52:26 -0700

    a little cleanup for ros-docs

--- osl-cookbooks/ros-docs/attributes/default.rb
+++ osl-cookbooks/ros-docs/attributes/default.rb
@@ -3,13 +3,12 @@ default['ros-docs']['user'] = "rosbot"
 default['ros-docs']['ssh_keys'] = nil
 default['ros-docs']['linkpath'] = "/home/#{node['ros-docs']['user']}/docs"
 
-# Localmount config
-default['ros-docs']['localmount'] = nil
-default['ros-docs']['mountpath'] = "/data"
-
 # Gluster config
 default['ros-docs']['glustervol'] = nil
-default['ros-docs']['glusterpath'] = node['ros-docs']['mountpath']
+
+# Local mount config
+default['ros-docs']['localmount'] = nil
+default['ros-docs']['mountpath'] = "/data"
 
 # Apache2 config
 default['ros-docs']['server_name'] = node['fqdn']
