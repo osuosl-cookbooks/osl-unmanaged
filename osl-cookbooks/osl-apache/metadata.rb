75fae15eb71669532b8124080deaf089731989a4
armiller <armiller@osuosl.org>
2013-09-13 11:43:00 -0700

    osl-apache cookbook

---
+++ osl-cookbooks/osl-apache/metadata.rb
@@ -0,0 +1,7 @@
+name             'osl-apache'
+maintainer       'OSU Open Source Lab'
+maintainer_email 'systems@osuosl.org'
+license          'Apache 2.0'
+description      'Installs/Configures osl-apache'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
