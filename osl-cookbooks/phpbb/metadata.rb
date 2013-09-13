546c3339b10f78354360ba31e791c83c4ae7fa6c
armiller <armiller@osuosl.org>
2013-09-13 11:22:38 -0700

    Adding phpbb cookbook

---
+++ osl-cookbooks/phpbb/metadata.rb
@@ -0,0 +1,7 @@
+name             'phpbb'
+maintainer       'OSU Open Source Lab'
+maintainer_email 'systems@osuosl.org'
+license          'Apache 2.0'
+description      'Installs/Configures phpbb'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
