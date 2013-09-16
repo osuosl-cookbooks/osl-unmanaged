da48fa32880d97de15f4fd0f5a98b2a8492def2c
armiller <armiller@osuosl.org>
2013-09-16 16:24:21 -0700

    phpbb memcache

---
+++ osl-cookbooks/phpbb/recipes/memcache.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: phpbb
+# Recipe:: memcache
+#
+# Copyright 2013, OSU Open Source Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+include_recipe "memcached"
+
+#package "php53u-pecl-memcache"
+
+service "memcached" do
+  action [:start,:enable]
+end
+
+template "/etc/php.d/memcache.ini" do
+  source "memcache.ini.erb"
+  mode 00644
+end
