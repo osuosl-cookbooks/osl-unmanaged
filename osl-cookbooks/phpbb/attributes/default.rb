ab1d7c8894d86debaa1ba27007fa14cb0aa51c7e
armiller <armiller@osuosl.org>
2013-09-19 17:52:25 -0700

    Gluster attributes and test vhost

--- osl-cookbooks/phpbb/attributes/default.rb
+++ osl-cookbooks/phpbb/attributes/default.rb
@@ -1,7 +1,7 @@
 default['phpbb']['vhosts'] = ['www.phpbb.com', 'test.phpbb.com', 'standard.include', 'pingdom.include']
 
 # Gluster config
-default['phpbb']['glustervol'] = "fs3.osuosl.org:/data/phpbb-data"
+default['phpbb']['glustervol'] = "fs3.osuosl.bak:/phpbb-data"
 
 # Local mount config
 default['phpbb']['mountpath'] = "/data"
