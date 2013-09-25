dc42a9966a26f6e3d61f236f3eb014a9b465d887
Sean Rettig <seanrettig@gmail.com>
2013-09-25 14:45:48 -0700

    ROS: Adding PHP packages for turtlebot.com

--- osl-cookbooks/ros/metadata.rb
+++ osl-cookbooks/ros/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures ros'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.8'
+version          '0.1.9'
 depends          'ros-docs'
 depends          'git'
 depends          'apache2'
