56ec32aa122d6e900fa5d2db61374f6d3f7d70c3
Rudy Grigar <rudy@grigar.net>
2013-10-05 15:22:38 -0700

    major bump for new base cookbook

--- osl-cookbooks/base/metadata.rb
+++ osl-cookbooks/base/metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'rudy@osuosl.org'
 license          'All rights reserved'
 description      'Installs/Configures base packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.7'
+version          '1.0.0'
+
 depends          'chef_handler'
 depends          'user'
 depends          'openssh'
