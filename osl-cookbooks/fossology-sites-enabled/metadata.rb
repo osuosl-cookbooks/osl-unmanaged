18d8eb586cf8a5b7667b1f0a76511260497f0de5
Rudy Grigar <rudy@grigar.net>
2013-10-11 14:27:45 -0700

    remove this cookbook

--- osl-cookbooks/fossology-sites-enabled/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'fossology-sites-enabled'
-maintainer       'OSU Open Source Lab'
-maintainer_email 'coreyg@osuosl.org'
-license          'Apache 2.0'
-description      'Installs/Configures fossology-sites-enabled'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
