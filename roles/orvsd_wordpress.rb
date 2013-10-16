2bda321103cf9c3de1536b638059021d85a84d12
armiller <armiller@osuosl.org>
2013-10-16 16:35:54 -0700

    Remove base_managed role from wordpress

--- roles/orvsd_wordpress.rb
+++ roles/orvsd_wordpress.rb
@@ -1,8 +1,10 @@
 name "orvsd_wordpress"
 description "orvsd wordpress web server role"
 run_list(
-  "role[base_managed]",
   "recipe[orvsd-web::wordpress]"
 )
 override_attributes(
+    "nginx" => {
+        "repo_source" => "nginx"
+    }
 )
