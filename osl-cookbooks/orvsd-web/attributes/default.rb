40a7b15a8249a93fd4975776edb6fada98019b63
armiller <armiller@osuosl.org>
2013-10-17 16:46:46 -0700

    Remove old user array and use databags instead

--- osl-cookbooks/orvsd-web/attributes/default.rb
+++ osl-cookbooks/orvsd-web/attributes/default.rb
@@ -5,10 +5,3 @@ default['orvsdweb']['static']['glusterpath'] = nil
 default['orvsdweb']['static']['glustervol'] = nil
 
 default['orvsdweb']['wordpress']['root'] = "/var/www"
-
-default['orvsdweb']['users'] = ["master", "juvenile", "helpdesk", "wesd", "threerivers", "salkeiz", "hmsc", 
-                                "nsantiam", "orenterprise", "mcminnville", "amity", "skol", "bendlp",
-                                "ddouglas", "pps", "skit", "phoenix", "beaverton", "ortrail", "soesd",
-                                "sps", "cal", "gresham", "astoria", "ode", "fernridge", "osl", "hillsboro",
-                                "ttsd", "sherman", "riverdale"]
-default['orvsdweb']['uid'] = "9101"
--- osl-cookbooks/orvsd-web/recipes/wordpress.rb
+++ osl-cookbooks/orvsd-web/recipes/wordpress.rb
@@ -6,20 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 
-include_recipe "orvsd-web::default"
 include_recipe "nginx::default"
 
-district_user = node['orvsdweb']['users']
-district_uid = node['orvsdweb']['uid'].to_i
+district_user = data_bag('orvsd-web')
 
 district_user.each do | dis_user |
-    user dis_user do
-        home "/home/#{dis_user}"
-        username "#{dis_user}"
-        uid "#{district_uid}"
+    district = data_bag_item('orvsd-web', dis_user)
+    homedir = "/home/#{dis_user}"
+
+    user(dis_user) do
+        home homedir
+        uid district['uid']
         shell "/bin/bash"
-        action :create
     end
+
     directory "#{node['orvsdweb']['wordpress']['root']}/#{dis_user}" do
         owner "alfred"
         group "alfred"
@@ -27,7 +27,6 @@ district_user.each do | dis_user |
         action :create
         recursive true
     end
-    district_uid += 1
 end
 
 
