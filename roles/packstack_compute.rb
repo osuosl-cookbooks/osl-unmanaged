0ef3f8e2effa9c2a06eacad00d40028515f6467f
Geoffrey Corey <coreyg@osuosl.org>
2013-12-16 16:23:35 -0800

    added role for packstack compute node

---
+++ roles/packstack_compute.rb
@@ -0,0 +1,12 @@
+name "packstack_compute"
+description "Main setup for packstack compute nodes"
+run_list  "recipe[osl-packstack]", "role[base_managed]"
+override_attributes "osl-packstack" => {
+  "rdo" => {
+    "release" => "havana"
+  },
+  "type" => "compute"
+}, "users" => [
+  "packstack-root",
+  "packstack-nova"
+]
