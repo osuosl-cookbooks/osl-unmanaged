4d844a83fb0f4bb8cc4ce3ff3e8f2cf11b940883
Geoffrey Corey <coreyg@osuosl.org>
2013-12-16 16:09:09 -0800

    role for non-compute packstack node + base_managed role in correct order

---
+++ roles/packstack.rb
@@ -0,0 +1,11 @@
+name "packstack"
+description "Main setup for packstack nodes"
+run_list  "recipe[osl-packstack]", "role[base_managed]"
+override_attributes "osl-packstack" => {
+  "rdo" => {
+    "release" => "havana"
+  },
+  "type" => "not_compute"
+}, "users" => [
+  "packstack-root"
+]
