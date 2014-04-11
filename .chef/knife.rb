668801ab8c9d6facefb48a2e1bcd319004d729fa
Jordan Evans <jordane@osuosl.org>
2014-04-11 10:02:02 -0700

    extra cookbook_paths breaks jenkins

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -17,7 +17,6 @@ cache_type              'BasicFile'
 cache_options( :path => "#{home_dir}/.chef/checksums" )
 cookbook_path [
                         "#{base_path}/osuosl-cookbooks",
-                        "#{base_path}/community-cookbooks"
               ]
 cookbook_copyright      "Oregon State University"
 cookbook_license        "apachev2"
