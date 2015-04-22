dfb33543d3aa0f49de35a9c3cc32dacdbc9485a3
Sean Rettig <seanrettig@gmail.com>
2015-04-22 12:52:29 -0700

    clean up, rubocop fixes

--- scripts/update_mr_osl_repos.rb
+++ scripts/update_mr_osl_repos.rb
@@ -6,15 +6,12 @@ client = Octokit::Client.new \
   :access_token => ARGV[0]
 
 client.auto_paginate = true
-user = client.user 'osuosl-cookbooks'
 
 entries = []
-
 client.org_repos('osuosl-cookbooks').collect { |r| r.name }.sort.each do |name|
-  puts name
   entries << "[osuosl-cookbooks/#{name}]\ncheckout = git clone git@github.com:osuosl-cookbooks/#{name}.git"
 end
 
-File.open("../.mrconfig.d/github-repos", "w") do |f|
+File.open('../.mrconfig.d/github-repos', 'w') do |f|
   f.puts entries.join("\n\n")
 end
