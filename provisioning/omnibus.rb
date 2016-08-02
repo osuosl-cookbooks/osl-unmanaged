8e2e7449d5615ab6e77d0713508908cb9ab0d16f
Lance Albertson <lance@osuosl.org>
2016-08-02 11:21:23 -0700

    Update omnibus chef provisioning

--- provisioning/omnibus.rb
+++ provisioning/omnibus.rb
@@ -3,17 +3,14 @@ require 'chef/provisioning/ssh_driver'
 
 with_driver 'ssh'
 
-# temporary workaround for a bug with chef-provisioning
-with_chef_server Chef::Config[:chef_server_url].sub('chefzero', 'http')
-
 machine_batch do
   action [:ready, :converge_only]
 #  action :converge
 #  action :destroy
   machine 'omnibus-ppc64' do
     machine_options transport_options: {
-      host: '140.211.168.45',
-      username: 'rhel',
+      host: '10.1.100.54',
+      username: 'centos',
       'ssh_options' => {
         keys: [
           '~/.ssh/id_rsa-work'
@@ -31,8 +28,8 @@ machine_batch do
 
   machine 'omnibus-ppc64le' do
     machine_options transport_options: {
-      host: '140.211.168.46',
-      username: 'rhel',
+      host: '10.1.100.55',
+      username: 'centos',
       'ssh_options' => {
         keys: [
           '~/.ssh/id_rsa-work'
