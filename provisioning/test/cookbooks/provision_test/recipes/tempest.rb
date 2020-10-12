026eee66f0401d2e0c059b8cb665a0c69032f67c
Lance Albertson <lance@osuosl.org>
2020-10-12 16:07:28 -0700

    Stein provisioning updates
    Updates for testing Stein with Cinc.

--- provisioning/test/cookbooks/provision_test/recipes/tempest.rb
+++ provisioning/test/cookbooks/provision_test/recipes/tempest.rb
@@ -1,15 +1,5 @@
 include_recipe 'openstack-integration-test::setup'
 
-edit_resource(:python_runtime, '2') do
-  provider :system
-  pip_version '9.0.3'
-end
-
-edit_resource(:python_virtualenv, '/opt/tempest-venv') do
-  system_site_packages true
-  pip_version '9.0.3'
-end
-
 execute 'Add network uuid' do
   command <<-EOF
     NET_ID=$(cat /var/tmp/public_network)
